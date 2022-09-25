3253b42547692f8b3ee7f51afd7ac85f055c5a61
Muhammad Anas Raza Siddiqui <anasattari48@gmail.com>
2022-09-26 02:36:17 +0500

    css updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
  import Login from './component/login'
 // import Signup from './component/signup'
-// import From from './component/from'
-
+import From from './component/from'
+import Nav from './component/navBar'
 import './App.css';
 
 function App() {
 return(
 <>
+<Nav/>
 <Login/>
 </>
   
