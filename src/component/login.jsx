f591dce10efb6481a4c96fa211569b65ac673f13
Muhammad Anas Raza Siddiqui <anasattari48@gmail.com>
2022-09-26 02:10:11 +0500

    error solve

--- src/component/login.jsx
+++ src/component/login.jsx
@@ -1,48 +1,47 @@
  import { useFormik } from 'formik';
  import * as yup from 'yup';
- import {Link} from "react-router-dom";
+//  import {Link} from "react-router-dom";
 import './login.css'
 
- function login (){
-
+ function Login (){
+  const validationSchema= yup.object({
+    email: yup
+      .string('Enter your email')
+      .email('Enter a valid email')
+      .required('Email is required'),
+    password: yup
+      .string('Enter your password')
+      .min(8, 'Password should be of minimum 8 characters length')
+      .required('Password is required'),
+  
+  })
     const formik = useFormik({
         initialValues: {
           email: '',
           password: '',
         },
-       validationSchema: yup.object({
-        email: yup
-          .string('Enter your email')
-          .email('Enter a valid email')
-          .required('Email is required'),
-        password: yup
-          .string('Enter your password')
-          .min(8, 'Password should be of minimum 8 characters length')
-          .required('Password is required'),
-      
-      }),
+       validationSchema: validationSchema,
         onSubmit: (values) => {
           console.log("values" ,values);
         },
       });
       return (
-        <div class="box">
-  <div class="title-box">
+        <div className="box">
+   <div className="title-box">
     <img src="https://i.postimg.cc/NMyj90t9/logo.png" alt="Facebook"/>
     <p>Facebook helps you connect and share with the people in your life.</p>
   </div>
-  <div class="form-box">
+  <div className="form-box">
           
           <form onSubmit={formik.handleSubmit}>
             <input
-             
-              id="email"
+            placeholder="Email address or phone number"
+                     id="email"
               name="email"
               label="Email"
               value={formik.values.email}
               onChange={formik.handleChange}
-              
-              
+
             />
             <br/>
             {(formik.touched.email && Boolean(formik.errors.email))?
@@ -51,7 +50,7 @@ import './login.css'
             
             <br/>
             <input
-              
+               placeholder="Password"
               id="password"
               name="password"
               label="Password"
@@ -70,20 +69,37 @@ import './login.css'
             <button color="primary" variant="contained" fullWidth type="submit">
             Log In
             </button>
-            <Link to="/">Forgotten Password</Link>
+            <a to="/">Forgotten Password</a>
           </form>
-        </div>
+        
         
 
         <hr/>
-    <div class="create-btn">
-      <Link to="/" target="_blank">Create New Account</Link>
+    <div className="create-btn">
+      <a to="/" >Create New Account</a>
     </div>
   </div>
+</div>
 
 
-
-
+/* <div className="box">
+  <div className="title-box">
+    <img src="https://i.postimg.cc/NMyj90t9/logo.png" alt="Facebook">
+    <p>Facebook helps you connect and share with the people in your life.</p>
+  </div>
+  <div className="form-box">
+    <form action="#">
+      <input type="text" placeholder="Email address or phone number">
+      <input type="password" placeholder="Password">
+      <button type="submit">Log In</button>
+      <a href="#">Forgotten Password</a>
+    </form>
+    <hr>
+    <div className="create-btn">
+      <a href="https://youtu.be/Lcw8t9vTpQI" target="_blank">Create New Account</a>
+    </div>
+  </div>
+</div>
 
 
 
@@ -93,11 +109,11 @@ import './login.css'
     //   <input type="password" placeholder="Password"/>
     //   <button type="submit">Log In</button>
     //   <Link to="/">Forgotten Password</Link>
-    // </form>
+    // </form> */
     
         
 
       );
  }
 
-   export default login ;
+   export default Login ;
